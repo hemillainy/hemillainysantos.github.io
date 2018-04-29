9933109e2419050cc990b687a0bc7e03484450b4
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2018-04-29 03:29:25 -0300

    add css, iniciada a imp do remove e alteraçoes do design

--- Mural/client.js
+++ Mural/client.js
@@ -1,7 +1,7 @@
 const listagem = document.getElementById("listagem");
 const titulo = document.getElementById("titulo");
 const mensagem = document.getElementById("mensagem");
-const author = document.getElementById("author");
+const autor = document.getElementById("autor");
 
 let msgs = [];
 
@@ -10,21 +10,21 @@ function update_msgs() {
     .then(r => r.json())
     .then(data => {
         Object.assign(msgs, data);
-        update_views(msgs.reverse());
+        update_views(msgs);
     });
 }
+
 function update_views(array) {
         const items = array.map(e => `
-        <div class="card">
+        <div class="card bg-light mb-3" style="max-width: 18rem; margin-right: 15px; height: 152px; width: 251px; overflow: auto">
+            <div class="card-header">${e.title}</div>
             <div class="card-body">
-                <h2 class="card-title">${e.title}</h2> 
-                <p class="card-text">${e.msg}</p> 
-                <p class="card-text">
+                <h6 class="card-title">${e.msg}</h6>
+                <p class="card-text"></p>
                     <small class="text-muted">${e.author}</small>
                     <small class="text-muted">${e.created_at}</small>
-                </p>
             </div>
-        </div><br>`).join("\n");
+        </div>`).join("\n");
         listagem.innerHTML = items;        
 };
 
@@ -34,7 +34,7 @@ function send() {
     body: JSON.stringify({
         title:titulo.value, 
         msg:mensagem.value, 
-        author:author.value, 
+        author:autor.value, 
         credentials:"hemillainy:friends"})
   })
   .then(dado => dado.json())
@@ -43,10 +43,8 @@ function send() {
 
 update_msgs();
 
-function clear_input(){
-    titulo.value = "";
-    mensagem.value = "";
-    author.value = "";
+function clear_input(tag){
+    tag.value = "";
 }
 
 function search(tag) {
@@ -56,4 +54,35 @@ function search(tag) {
 
 function show(document){
     document.hidden = !document.hidden;   
+}
+
+function get_minhas_msgs() {
+    let mensagens = msgs.filter(e => e.frontend === "hemillainy");
+    console.log(mensagens);
+    minhas_msgs(mensagens);
+}
+
+function minhas_msgs(mensagens) {
+    const items = mensagens.map(e => `
+    <div class="card bg-light mb-3" style="max-width: 18rem; margin-right: 15px; height: 152px; width: 251px; overflow: auto">
+        <button type="button" class="close" aria-label="Close" onclick="apagar()">
+            <span aria-hidden="true" style="padding-left: 220px">&times;</span>
+        </button>
+        <div class="card-header">${e.title}</div>
+        <div class="card-body">
+        <h6 class="card-title">${e.msg}</h6>
+        <p class="card-text"></p>
+            <small class="text-muted">${e.author}</small>
+            <small class="text-muted">${e.created_at}</small>
+        </div>
+    </div>`).join("\n");
+    listagem.innerHTML = items;        
+};
+
+function apagar(){
+    fetch("http://150.165.85.16:9900/api/msgs/:id", {
+    method: "delete",
+    body: JSON.stringify({credentials:"hemillainy:friends"})
+    })
+    .then();
 }
